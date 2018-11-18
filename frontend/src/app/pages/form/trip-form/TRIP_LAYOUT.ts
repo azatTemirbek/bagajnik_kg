ac118985b9dc5867ed4349308551ace1611a97b4
Azat Temirbek uulu <e178551@gmail.com>
2018-11-18 18:04:27 +0300

    layout form

---
+++ frontend/src/app/pages/form/trip-form/TRIP_LAYOUT.ts
@@ -0,0 +1,42 @@
+export const MY_FORM_LAYOUT = {
+  'from_formatted_address': {
+    element: {
+      container: 'form-group d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center',
+      label: 'col-form-label',
+    },
+    grid: {
+      control: 'col-sm-9 col-md-6 col-lg-4',
+      label: 'col-sm-3 col-form-label',
+    }
+  },
+  'to_formatted_address': {
+    element: {
+      container: 'form-group d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center',
+      label: 'col-form-label'
+    },
+    grid: {
+      control: 'col-sm-9 col-md-6 col-lg-4',
+      label: 'col-sm-3 col-form-label'
+    }
+  },
+  'start_dt': {
+    element: {
+      container: 'form-group d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center',
+      label: 'col-form-label'
+    },
+    grid: {
+      control: 'col-sm-9 col-md-6 col-lg-4',
+      label: 'col-sm-3 col-form-label'
+    }
+  },
+  'end_dt': {
+    element: {
+      container: 'form-group d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center',
+      label: 'col-form-label'
+    },
+    grid: {
+      control: 'col-sm-9 col-md-6 col-lg-4',
+      label: 'col-sm-3 col-form-label'
+    }
+  }
+};
