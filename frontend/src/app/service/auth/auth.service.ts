554cde8b8df267e7ddefd45570222bcfc1db0b21
Azat Temirbek uulu <e178551@gmail.com>
2018-11-22 01:47:46 +0300

    todo

--- frontend/src/app/service/auth/auth.service.ts
+++ frontend/src/app/service/auth/auth.service.ts
@@ -8,6 +8,6 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   loggedIn: BehaviorSubject<boolean> = new BehaviorSubject(this.Token.loggedIn());
   // todo
-  me: BehaviorSubject<any> = new BehaviorSubject({ id: 2 });
+  me: BehaviorSubject<any> = new BehaviorSubject({});
   constructor(private Token: TokenService) { }
 }
