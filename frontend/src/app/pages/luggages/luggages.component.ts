2412bfcc6be27d8dcbbba31f5e36d5db00770021
Azat <12012594+e178551@users.noreply.github.com>
2018-11-21 17:00:30 +0300

    google l

--- frontend/src/app/pages/luggages/luggages.component.ts
+++ frontend/src/app/pages/luggages/luggages.component.ts
@@ -15,6 +15,7 @@ import { Subscription } from 'rxjs';
 import { LuggageService } from 'src/app/service/luggage.service';
 import { Options, LabelType } from 'ng5-slider';
 import { MapsAPILoader } from '@agm/core';
+declare var google; 
 
 @Component({
   selector: 'app-luggage',
