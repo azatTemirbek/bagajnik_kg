2b5e0d446821a04cc3b48098322ebd9e791d5310
aizat09 <aizat09>
2018-11-25 22:28:04 +0600

    profile

--- frontend/src/app/service/auth/auth.service.ts
+++ frontend/src/app/service/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { TokenService } from 'src/app/service/auth/token.service';
 import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Configure} from "../configure";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+    api = Configure.api();
   loggedIn: BehaviorSubject<boolean> = new BehaviorSubject(this.Token.loggedIn());
 
   me: BehaviorSubject<any> = new BehaviorSubject({});
-  constructor(private Token: TokenService) { }
+  constructor(
+      private Token: TokenService,
+      private http: HttpClient,
+      ) { }
+    read(id: Number) {
+        return this.http.get(`${this.api}/user/${id}`);
+    }
+    update(data) {
+        return this.http.post(`${this.api}/user/${data.id}`, data);
+    }
 }
